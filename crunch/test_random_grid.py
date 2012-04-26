69d6490aa91e87401db7988b90ef7ab941141f18
E. G. Patrick Bos <pbos@astro.rug.nl>
2012-04-26 16:23:00 +0200

    Bestand verplaatst.

--- crunch/test_random_grid.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import pyublas, egp.crunch as c
-from timeit import Timer
-
-t1 = Timer("array=c.resolution_independent_random_grid(16,0)", "import pyublas; import egp.crunch as c")
-t1.timeit(1000)
-
-array=c.resolution_independent_random_grid(16,0)
