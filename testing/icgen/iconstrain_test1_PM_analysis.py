027331c09494dacbec983f73fbadf2f85a052ce5
E. G. Patrick Bos <patrick@bosbook>
2012-12-20 13:57:23 +0100

    fix iconstrain_pm test

--- testing/icgen/iconstrain_test1_PM_analysis.py
+++ testing/icgen/iconstrain_test1_PM_analysis.py
@@ -9,9 +9,9 @@ run_path_base = '/Users/users/pbos/dataserver/cubep3m/iconstrain_scratch/'
 run_name = 'step'
 simulation = egp.io.CubeP3MData(run_path_base+run_name+'/output/0.000xv0.dat')
 radius = 4.0225468329142506
-spheregrid = iconstrain.get_peak_particle_indices(target_pos, radius, boxlen, gridsize_iter)
+spheregrid = iconstrain.get_peak_particle_indices(target_pos, radius, boxlen, gridsize_iter).reshape(simulation.pos.shape[0])
 
-mean_peak_pos = simulation.pos.T.reshape(3,gridsize_iter, gridsize_iter, gridsize_iter)[:,spheregrid].mean(axis=1)
+mean_peak_pos = simulation.pos[:,spheregrid].mean(axis=1)
 
 pid_filename = simulation.filename[:simulation.filename.find('xv')]+'PID0.dat'
 idarray = np.memmap(pid_filename, dtype='int64', offset=simulation.offset)
